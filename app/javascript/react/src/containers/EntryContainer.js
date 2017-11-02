8de5a5924a5b5a94c89ee2b02bc9276cfe8944e9
Serena Entezary <serena.entezary@gmail.com>
2017-11-02 13:58:23 -0400

    EntryContainer updated

--- app/javascript/react/src/containers/EntryContainer.js
+++ app/javascript/react/src/containers/EntryContainer.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import UserEmotionContainer from './UserEmotionContainer'
+import JournalContainer from './JournalContainer'
+import GoalsContainer from './GoalsContainer'
 
-class EntryContainer extends Component{
+class EntryContainer extends Component {
  constructor(props) {
    super(props);
    this.state = {
+     currentUser: [],
+     totalEntry: []
    }
  }
+
+ componentDidMount() {
+   fetch('/api/v1/user/is_signed_in.json', {
+     credentials: 'same-origin',
+     method: 'GET',
+     headers: { 'Content-Type': 'application/json' }
+   })
+   .then(response => response.json())
+   .then(body => {
+     this.setState({ currentUser: body.user })
+   })
+ }
+
  render() {
 
    return(
      <div>
+       <JournalContainer
+         currentUser={this.state.currentUser}
+       />
+       <UserEmotionContainer
+         currentUser={this.state.currentUser}
+       />
      </div>
    )
  }
