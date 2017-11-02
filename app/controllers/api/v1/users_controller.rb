508403c70f2fc209922307bae200ca633e591952
Serena Entezary <serena.entezary@gmail.com>
2017-11-02 13:56:20 -0400

    users controller updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
   def index
-    user = current_user
-    render json: user
+    users = User.all
+  end
+
+  def is_signed_in?
+    if user_signed_in?
+      render :json => {"signed_in" => true, "user" => current_user}
+    else
+      render :json => {"signed_in" => false}
+    end
   end
 end
