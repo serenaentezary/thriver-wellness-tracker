6f6fe868467ecde7c525334512290049f6669095
Serena Entezary <serena.entezary@gmail.com>
2017-11-02 13:59:08 -0400

    updated App (React Router)

--- app/javascript/react/src/containers/App.js
+++ app/javascript/react/src/containers/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import UserEmotionContainer from './UserEmotionContainer'
-
+import IndexContainer from './IndexContainer'
+import JournalContainer from './JournalContainer'
 
 const App = props => {
   return(
     <BrowserRouter>
       <div>
-        <Route exact path='/user_emotions' component={UserEmotionContainer} />
+        <Route exact path='/' component={IndexContainer} />
       </div>
     </BrowserRouter>
   )
