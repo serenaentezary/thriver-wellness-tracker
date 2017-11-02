cd62dfebaa540b5d7250617361e68ff0062a6ea1
Serena Entezary <serena.entezary@gmail.com>
2017-11-02 13:48:38 -0400

    Still updating/adding some files

---
+++ app/javascript/react/src/containers/IndexContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import EntryContainer from './EntryContainer'
+
+class IndexContainer extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     selectedSection: null,
+     entrySections: []
+   }
+ }
+
+ render() {
+    return(
+      <div>
+        <EntryContainer />
+      </div>
+    )
+  return(
+      <div>
+        {entries}
+      </div>
+    )
+  }
+}
+export default IndexContainer
