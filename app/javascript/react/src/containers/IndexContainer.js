90ab61cc96c4ce5b71da9fa8d603ef77cb28c769
Serena Entezary <serena.entezary@gmail.com>
2017-11-13 17:08:00 -0500

    Added the small-8 columns to Recent Articles

--- app/javascript/react/src/containers/IndexContainer.js
+++ app/javascript/react/src/containers/IndexContainer.js
@@ -389,7 +389,7 @@ class IndexContainer extends Component {
           </div>
         </div>
           <div className="row">
-            <div className="large-8 columns">
+            <div className="large-8 small-8 columns">
               <h3 className="recent-articles-title">Recent Articles</h3>
               <div className="article-link"><a href={this.state.randomArticle1[0]}>{this.state.randomArticle1[1]}</a></div>
               <div className="article-link"><a href={this.state.randomArticle2[0]}>{this.state.randomArticle2[1]}</a></div>
